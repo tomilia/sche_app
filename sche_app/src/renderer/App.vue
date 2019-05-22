97c86cc8c42458a0d154d67971a13012c864d373
tomilia <koekax@gmail.com>
2019-05-22 18:47:21 +0800

    init

---
+++ sche_app/src/renderer/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sche_app'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
