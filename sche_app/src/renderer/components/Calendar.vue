1aa17bd39b29416562e4043fdb903f9b2aed6bc9
tomilia <koekax@gmail.com>
2019-08-04 05:21:10 +0800

    Calendar added

--- sche_app/src/renderer/components/Calendar.vue
+++ sche_app/src/renderer/components/Calendar.vue
@@ -1,16 +1,68 @@
 <template>
- <div>
-     <h1>Under-constructino</h1>
- </div>
-</template>
-
-<script>
-  export default {
-    name: 'Calendar',
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
+      <div id="app">
+        <div class="main">
+          <div class="calendar-holder">
+              <calendar 
+    :events='events'
+      />
+          </div>
+          
+          <div class="form-holder">
+            <h3>Schedule an event</h3>
+            <event-form />
+          </div>
+          
+        </div>
+      </div>
+    </template>
+    <script>
+    import {db} from '../db'
+    import Calendar from './FullCalendar.vue'
+    import EventForm from './Form.vue'
+    import Pusher from 'pusher-js';
+    export default {
+      name: 'app',
+      components: {
+        Calendar,
+        EventForm
+      },
+      created(){
+       this.$bind('events', db.collection('schedule'))
+      },
+      
+      data(){
+        return {
+        events:[],
+        
+        }
+      },
+      
     }
-  }
-</script>
+    </script>
+    <style>
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+    }
+    .main {
+      display: flex;
+      align-items: center;
+    }
+    .calendar-holder {
+      width: 65%;
+    }
+    .form-holder {
+      width: 35%;
+    }
+    .form-holder > h3 {
+      color: orangered;
+      text-transform: uppercase;
+      font-size: 16px;
+      text-align: left;
+      margin-left: 30px;
+      margin-bottom: 10px;
+    }
+    </style>
---
+++ sche_app/src/renderer/components/FullCalendar.vue
@@ -0,0 +1,68 @@
+<template>
+      <FullCalendar
+    defaultView="dayGridMonth" :plugins="calendarPlugins"
+    @dateClick="handleDateClick"
+    @eventClick="myEventSelected"
+    :events='events'
+      />
+    </template>
+
+    <script>
+    import FullCalendar from '@fullcalendar/vue';
+    import dayGridPlugin from '@fullcalendar/daygrid';
+    import interactionPlugin from '@fullcalendar/interaction';
+    export default {
+      name: 'Calendar',
+      props: ['events'],
+      components: {
+        FullCalendar
+      },
+      data(){
+          return{
+          calendarPlugins: [ dayGridPlugin, interactionPlugin ]
+          }
+      },
+      methods:{
+          handleDateClick(arg) {
+    console.log(arg.date)
+  },
+         myEventSelected(event, jsEvent, pos) {
+        console.log('eventClick', event, jsEvent, pos)
+      }
+      },
+     
+
+    }
+    </script>
+
+    <style>
+    @import '~@fullcalendar/core/main.css';
+    @import '~@fullcalendar/daygrid/main.css';
+    .red {
+      background: rgb(235, 77, 77) !important;
+      color: whitesmoke !important;
+    }
+    .blue {
+      background: rgb(59, 59, 163) !important;
+      color: whitesmoke !important;
+    }
+    .orange {
+      background: orange !important;
+      color: white !important;
+    }
+    .green {
+      background: rgb(49, 155, 49) !important;
+      color: white !important;
+    }
+    .blue,
+    .orange,
+    .red,
+    .green {
+      font-size: 13px;
+      font-weight: 500;
+      text-transform: capitalize;
+    }
+    .event-item {
+      padding: 2px 0 2px 4px !important;
+    }
+    </style>
